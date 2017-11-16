8a4fcbead4a3176ef1a695ccee7d080e5d159dcf
Hans-Uwe Brackel <hbrackel@iCloud.com>
2017-11-16 11:30:54 -0600

    converted ConfigurationException to Kotlin

--- src/main/kotlin/de/macnix/util/configuration/ConfigurationException.kt
+++ src/main/kotlin/de/macnix/util/configuration/ConfigurationException.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016 Hans-Uwe Brackel
+ * Copyright (c) 2017 Hans-Uwe Brackel
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
@@ -16,8 +16,4 @@
 
 package de.macnix.util.configuration
 
-class ConfigurationException extends Exception {
-    ConfigurationException(String message) {
-        super(message)
-    }
-}
+class ConfigurationException(message: String) : Exception(message)
