fb0f6d737596fc0695fd0cbd00b2a6c64cf1e0ff
Hans-Uwe Brackel <hbrackel@iCloud.com>
2019-04-27 04:33:29 +0200

    Extract dependency versions into buildSrc object

---
+++ buildSrc/src/main/kotlin/Deps.kt
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2019 Hans-Uwe Brackel
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+object Deps {
+
+    // dependencies
+
+    // kotlin
+    val kotlinVersion = "1.3.31"
+    val log4jVersion = "2.11.2"
+
+    // logging
+    val slf4jApiVersion = "1.7.25"
+    val logbackClassicVersion = "1.2.3"
+
+    //# vertx
+    val vertxVersion = "3.5.1"
+
+    val jacksonVersion = "2.9.5"
+    val jacksonCsvVersion = "2.9.5"
+    val apacheCommonsCsvVersion = "1.5"
+
+    //# test environment
+    val junitPlatformVersion = "1.0.2"
+    val junitJupiterVersion = "5.0.2"
+    val cucumberVersion = "1.2.5"
+    val mockitoVersion = "2.13.0"
+    val spekVersion = "1.1.5"
+    val hamcrestAllVersion = "1.3"
+    val awaitilityVersion = "1.7.0"
+
+    //# spock framework related
+    val byteBuddyVersion = "1.8.12"
+    val objenesisVersion = "2.6"
+    val spockCoreVersion = "1.1-groovy-2.4"
+
+    val mockkVersion = "1.9"
+    val assertjVersion = "3.11.1"
+
+    // DI
+    val springVersion = "5.0.2.RELEASE"
+
+    val uomConverterVersion = "0.1+"
+    val reactorCoreVersion = "3.1.2.RELEASE"
+
+
+}
