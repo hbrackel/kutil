b538d4a464bf56797212164aff9b832bf76f705b
Hans-Uwe Brackel <hbrackel@iCloud.com>
2019-05-02 19:02:05 +0200

    Use kotlin-conventions and ci-publishing-conventions plugins from gradle-build-scripts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,8 @@
 plugins {
-    id("java-library")  // needed to make dependencies 'implementation' etc be resolved
-    id("org.jetbrains.kotlin.jvm") version "1.3.31"
-    id("org.sonarqube") version "2.7"
-    id("maven-publish")
+    id("kotlin-conventions")
+    id("ci-publishing-conventions")
     id("de.macnix.gitversion") version "1.0.3"
-    id("de.macnix.versionfile")
 }
-
-apply(from = "$rootDir/testEnvironment.gradle")
 
 repositories {
     maven { url = upstreamRepoUrl() }
@@ -17,8 +12,6 @@ repositories {
 group = "de.macnix.util"
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-
     implementation("org.slf4j:slf4j-api:${Deps.slf4jApiVersion}")
     implementation("com.fasterxml.jackson.core:jackson-databind:${Deps.jacksonVersion}")
     implementation("io.projectreactor:reactor-core:${Deps.reactorCoreVersion}")
@@ -44,37 +37,5 @@ publishing {
                 }
             }
         }
-    }
-    repositories {
-        maven {
-            name = "external"
-            url = libsReleaseLocalRepoUrl()
-            credentials {
-                username = deployUser()
-                password = deployPassword()
-            }
-        }
-        maven {
-            name = "downstream"
-            url = downstreamRepoUrl()
-        }
-    }
-}
-
-tasks {
-    compileKotlin {
-        sourceCompatibility = JavaVersion.VERSION_1_8.toString()
-        targetCompatibility = JavaVersion.VERSION_1_8.toString()
-
-        kotlinOptions {
-            jvmTarget = "1.8"
-            allWarningsAsErrors = true
-        }
-    }
-    compileTestKotlin {
-        kotlinOptions {
-            jvmTarget = "1.8"
-        }
-
     }
 }
