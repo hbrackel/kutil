fb03fdcc9b5b926a64ff79ee382819ad9c5dc837
Hans-Uwe Brackel <hbrackel@iCloud.com>
2020-05-04 15:33:41 +0200

    upgradle gitversion plugin to v1.1.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("kotlin-conventions")
     id("ci-publishing-conventions")
-    id("de.macnix.gitversion") version "1.0.5"
+    id("de.macnix.gitversion") version "1.1.0"
 }
 
 repositories {
