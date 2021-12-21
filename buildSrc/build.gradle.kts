2c75fad41847f2172322a2ba279df0dfcc9cbda5
Hans-Uwe Brackel <hbrackel@googlemail.com>
2021-12-21 04:58:01 +0100

    upgradle gradle kotlin plugin to 1.6.10

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -17,5 +17,5 @@ repositories {
 
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.30")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
 }
