886dacd6ac9c5c0a9b978754c8dd2bd0806ae68b
Hans-Uwe Brackel <hbrackel@icloud.com>
2022-05-19 11:51:20 +0200

    update to latest maven dependencies (2022.05.19-1)

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -17,5 +17,5 @@ repositories {
 
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.21")
 }
